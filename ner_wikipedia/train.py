726db51f563779d5fabded26571c7b372e225364
ftnext <takuyafjp+develop@gmail.com>
2021-02-23 15:35:41 +0900

    [feat] Support BidirectionalModel (10-6-2)

--- ner_wikipedia/train.py
+++ ner_wikipedia/train.py
@@ -1,19 +1,31 @@
+import argparse
+
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.models import load_model
 from seqeval.metrics import classification_report
 
 from inference import InferenceAPI
-from models import UnidirectionalModel
+from models import BidirectionalModel, UnidirectionalModel
 from preprocessing import create_dataset, preprocess_dataset, Vocab
 from utils import load_dataset
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "model_type", choices=("unidirectional", "bidirectional")
+    )
+    args = parser.parse_args()
+
     batch_size = 32
     epochs = 100
-    model_path = "models/unidirectional_model.h5"
+    model_path = f"models/{args.model_type}_model.h5"
     num_words = 15000
+
+    model_class = BidirectionalModel
+    if args.model_type == "unidirectional":
+        model_class = UnidirectionalModel
 
     x, y = load_dataset("./data/ja.wikipedia.conll")
 
@@ -26,7 +38,7 @@ if __name__ == "__main__":
     x_train = create_dataset(x_train, source_vocab)
     y_train = create_dataset(y_train, target_vocab)
 
-    model = UnidirectionalModel(num_words, target_vocab.size).build()
+    model = model_class(num_words, target_vocab.size).build()
     model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
 
     callbacks = [
