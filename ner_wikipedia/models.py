97eae99ae78d6ebf8737e09bb630ce44c3cbae46
ftnext <takuyafjp+develop@gmail.com>
2021-02-23 15:34:36 +0900

    [feat] Add model using Bi-LSTM (10-6-1)

--- ner_wikipedia/models.py
+++ ner_wikipedia/models.py
@@ -1,9 +1,15 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, Input, Embedding, LSTM
+from tensorflow.keras.layers import (
+    Bidirectional,
+    Dense,
+    Embedding,
+    Input,
+    LSTM,
+)
 
 
 class UnidirectionalModel:
-    """LSTMを使ったモデルを作成するためのクラス"""
+    """LSTMを使って、固有表現認識モデルを作成するためのクラス"""
 
     def __init__(self, input_dim, output_dim, emb_dim=100, hid_dim=100):
         self.input = Input(shape=(None,), name="input")
@@ -20,5 +26,28 @@ class UnidirectionalModel:
         x = self.input
         embedding = self.embedding(x)
         lstm = self.lstm(embedding)
+        y = self.fc(lstm)
+        return Model(inputs=x, outputs=y)
+
+
+class BidirectionalModel:
+    """Bi-LSTMを使って、固有表現認識モデルを作成するためのクラス"""
+
+    def __init__(self, input_dim, output_dim, emb_dim=100, hid_dim=100):
+        self.input = Input(shape=(None,), name="input")
+        self.embedding = Embedding(
+            input_dim=input_dim,
+            output_dim=emb_dim,
+            mask_zero=True,
+            name="embedding",
+        )
+        lstm = LSTM(hid_dim, return_sequences=True, name="lstm")
+        self.bilstm = Bidirectional(lstm, name="bilstm")
+        self.fc = Dense(output_dim, activation="softmax")
+
+    def build(self):
+        x = self.input
+        embedding = self.embedding(x)
+        lstm = self.bilstm(embedding)
         y = self.fc(lstm)
         return Model(inputs=x, outputs=y)
