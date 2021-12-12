a5b0f972da8264d7294211d7ddef92bfa4dcf015
ftnext <takuyafjp+develop@gmail.com>
2021-12-12 21:27:17 +0900

    [refactor] Rewrite with lightning to make code easier to handle

--- ner_wikipedia/torch_tutorial.py
+++ ner_wikipedia/torch_tutorial.py
@@ -4,32 +4,57 @@ https://pytorch.org/tutorials/beginner/nlp/sequence_models_tutorial.html
 
 from __future__ import annotations
 
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.data import IterableDataset, DataLoader
 
 
-class LSTMTagger(nn.Module):
+class LSTMTagger(pl.LightningModule):
     def __init__(
         self,
         embedding_dim: int,
         hidden_dim: int,
         vocab_size: int,
         tagset_size: int,
-    ):
+    ) -> None:
         super(LSTMTagger, self).__init__()
 
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim)
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
+        self.loss_function = nn.NLLLoss()
+
     def forward(self, sentence: "torch.Tensor") -> "torch.Tensor":
-        embeds = self.word_embeddings(sentence)
-        lstm_out, _ = self.lstm(embeds.view(len(sentence), 1, -1))
-        tag_space = self.hidden2tag(lstm_out.view(len(sentence), -1))
+        # LightningDataModuleにする前 sentense.size は 4や5
+        # LightningDataModuleにした後 sentense.size は (1, 4) や (1, 5)
+        embeds = self.word_embeddings(sentence.view(-1))
+        lstm_out, _ = self.lstm(embeds.view(sentence.size(1), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(sentence.size(1), -1))
         tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
+
+    def configure_optimizers(self):
+        optimizer = optim.SGD(self.parameters(), lr=0.1)
+        return optimizer
+
+    def training_step(self, train_batch, batch_ids):
+        # DataLoaderのbatch_sizeが1（デフォルトのため）batch_idsはint (0, 1)
+        sentence_in, targets = train_batch
+        tag_scores = self(sentence_in)
+        loss = self.loss_function(tag_scores, targets.view(-1))
+        return {"loss": loss}
+
+    def training_epoch_end(self, training_step_outputs):
+        mean_loss = torch.tensor(
+            [o["loss"] for o in training_step_outputs]
+        ).mean()
+        self.log("loss", mean_loss)
 
 
 def prepare_sequence(seq: list[str], to_ix: dict[str, int]) -> "torch.Tensor":
@@ -60,9 +85,36 @@ tag_to_ix = {"DET": 0, "NN": 1, "V": 2}
 ix_to_tag = {v: k for k, v in tag_to_ix.items()}
 
 
+class SentenceTagsPairDataset(IterableDataset):
+    def __init__(self):
+        super(SentenceTagsPairDataset, self).__init__()
+
+        sentence_tensors, tags_tensors = [], []
+        for sentence, tags in training_data:
+            sentence_tensors.append(prepare_sequence(sentence, word_to_ix))
+            tags_tensors.append(prepare_sequence(tags, tag_to_ix))
+        self.sentence_tensors = sentence_tensors
+        self.tags_tensors = tags_tensors
+
+    def __iter__(self):
+        return zip(self.sentence_tensors, self.tags_tensors)
+
+
+class PosTaggingExampleDataModule(pl.LightningDataModule):
+    def __init__(self) -> None:
+        super(PosTaggingExampleDataModule, self).__init__()
+
+    def setup(self, stage: str | None) -> None:
+        self.example_train = SentenceTagsPairDataset()
+
+    def train_dataloader(self):
+        return DataLoader(self.example_train)
+
+
 def check_with_sample(sentence: list[str]) -> None:
     inputs = prepare_sequence(sentence, word_to_ix)
-    tag_scores = model(inputs)
+    # inputs を PosTaggingExampleDataModule が返す形に変換
+    tag_scores = model(inputs.view(-1, len(inputs)))
     print(tag_scores)
     max_args = torch.argmax(tag_scores, dim=1)
     print("Sentence:", " ".join(sentence))
@@ -78,25 +130,16 @@ if __name__ == "__main__":
     model = LSTMTagger(
         EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix)
     )
-    loss_function = nn.NLLLoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
 
     with torch.no_grad():
         print("Before training:")
         check_with_sample(training_data[0][0])
 
-    for epoch in range(300):
-        for sentence, tags in training_data:
-            model.zero_grad()
-
-            sentence_in = prepare_sequence(sentence, word_to_ix)
-            targets = prepare_sequence(tags, tag_to_ix)
-
-            tag_scores = model(sentence_in)
-
-            loss = loss_function(tag_scores, targets)
-            loss.backward()
-            optimizer.step()
+    callbacks = [EarlyStopping("loss")]
+    trainer = pl.Trainer(callbacks=callbacks)
+    dm = PosTaggingExampleDataModule()
+    # 以下で、batchごとに optimizer.zero_grad() が呼ばれるので model.zero_grad() は消せる
+    trainer.fit(model, datamodule=dm)
 
     with torch.no_grad():
         print("After training:")
